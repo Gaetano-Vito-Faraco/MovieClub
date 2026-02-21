b4bb5d7c61641de351f57c81cf09209607edadab
Gaetano Vito Faraco <faracojob23@gmail.com>
2026-02-21 18:58:47 +0100

    Add files via upload

---
+++ DatabaseMC.sql
@@ -0,0 +1,65 @@
+DROP DATABASE if exists persistenzaMC;
+CREATE DATABASE persistenzaMC;
+USE persistenzaMC;
+
+
+CREATE TABLE Persona(
+ID int PRIMARY KEY auto_increment,
+Email varchar(50) UNIQUE NOT NULL,
+Password VARCHAR(44) NOT NULL,
+Nome varchar(30) NOT NULL,
+Cognome varchar(30) NOT NULL,
+Admin boolean NOT NULL
+);
+
+CREATE TABLE Film(
+ID int PRIMARY KEY auto_increment,
+Titolo varchar(60) NOT NULL,
+Descrizione varchar(220) NOT NULL,
+DataUscita date,
+Regista varchar(60),
+Durata time, 
+Genere varchar(100) NOT NULL,
+Copertina varchar(150), 
+Trailer varchar(100)
+);
+
+CREATE TABLE watchlist(
+ID_Persona int,
+ID_Film int,
+PRIMARY KEY (ID_Persona, ID_Film),
+FOREIGN KEY (ID_Persona) references Persona (ID),
+FOREIGN KEY (ID_Film) references Film (ID)
+);
+
+CREATE TABLE Lista(
+ID int auto_increment PRIMARY KEY,
+Nome varchar(30) NOT NULL,
+Privata boolean NOT NULL,
+Descrizione varchar(100),
+Email_Persona varchar(50) NOT NULL,
+FOREIGN KEY (Email_Persona) references Persona (Email)
+);
+
+CREATE TABLE Include(
+ID_Lista int,
+ID_Film int,
+PRIMARY KEY (ID_Lista, ID_Film),
+FOREIGN KEY (ID_Lista) references Lista (ID),
+FOREIGN KEY (ID_Film) references Film (ID)
+);
+
+CREATE TABLE Recensione(
+Valutazione int NOT NULL,
+Descrizione varchar(250),
+DataInserimento date NOT NULL,
+Email_Persona varchar(50),
+ID_Film int,
+PRIMARY KEY (Email_Persona, ID_Film),
+FOREIGN KEY (Email_Persona) references Persona (Email),
+FOREIGN KEY (ID_Film) references Film (ID)
+);
+
+INSERT INTO Persona (Email, Password, Nome, Cognome, Admin)
+VALUES ('prova1@gmail.com', sha1('user1234'), 'Mario', 'Rossi', 0),
+       ('admin@gmail.com', sha1('admin1234'), 'Admin', 'Admin', 1);
---
+++ README.md
@@ -0,0 +1,13 @@
+# MOVIECLUB 
+Progetto realizzato per il corso di Ingegneria del software dell'Università di Salerno (2023/2024).
+
+# Documentazione
+Tutti la documentazione relativa ai prodotti è disponibile nella cartella DOCUMENTI.
+- [Documenti] (https://github.com/salurso/MovieClub_NC07/tree/master/DOCUMENTI)
+- [Manuali] (https://github.com/salurso/MovieClub_NC07/tree/master/DOCUMENTI/5.%20MANUALI)
+
+# Autori
+- Andrea Salurso
+- Costantino Paciello
+- Gaetano Vito Faraco
+- Vittorio Guida
